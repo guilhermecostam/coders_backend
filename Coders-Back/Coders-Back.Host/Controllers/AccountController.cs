1ac9ba52ed264ee8854708a3634372226613d579
Jonathan Maia <jtauan10@gmail.com>
2023-05-20 23:18:21 -0300

    AccountController.cs updated

--- Coders-Back/Coders-Back.Host/Controllers/AccountController.cs
+++ Coders-Back/Coders-Back.Host/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Coders_Back.Host.Controllers;
 
 [ApiController]
+[Route("account")]
 public class AccountController : ControllerBase
 {
     private readonly IIdentityService _identityService;
@@ -17,13 +18,24 @@ public class AccountController : ControllerBase
     [HttpPost("register")]
     public async Task<IActionResult> Register([FromBody] RegisterInput input)
     {
-        //TODO: Trocar tipo de retorno ?
-        return Ok(await _identityService.Register(input));
+        //don't use this because [ApiController] attribute already does that
+        // if (!ModelState.IsValid)
+        //     return BadRequest();
+        
+        var registerResult = await _identityService.Register(input);
+        
+        if (registerResult.Success)
+            return Ok(registerResult);
+
+        return BadRequest(registerResult);
     }
     
     [HttpPost("login")]
     public async Task<IActionResult> Login([FromBody] LoginInput input)
     {
-        return Ok(await _identityService.Login(input));
+        var loginResult = await _identityService.Login(input);
+        if (loginResult.Success)
+            return Ok(loginResult);
+        return Unauthorized(loginResult);
     }
 }
