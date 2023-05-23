95bcd6ade59f05ac2727e2a888aa32fe65de55af
Jonathan Maia <jtauan10@gmail.com>
2023-05-22 21:37:39 -0300

    IdentityServiceTestsUtils.cs created #7

--- Coders-Back/Coders-Back.UnitTest/Infrastructure/IdentityServiceTests.cs
+++ Coders-Back/Coders-Back.UnitTest/Infrastructure/IdentityServiceTests.cs
@@ -1,11 +1,6 @@
-using Coders_Back.Domain.DTOs.Input;
 using Coders_Back.Domain.Entities;
-using Coders_Back.Infrastructure.Identity.Configurations;
 using Coders_Back.Infrastructure.Identity.Services;
 using FluentAssertions;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Options;
 using Moq;
 using Xunit;
 
@@ -16,55 +11,21 @@ public class IdentityServiceTests
     [Fact(DisplayName = "Try to register user with invalid email")]
     public async void RegisterInvalidEmail()
     {
-        //TODO: performe all arranges and setups in Utils Class
-        //Arrange
-        var registerInput = new RegisterInput("amazingemail.something", "An amazing person", "AInvalidPassword",
-            "AInvalidPassword");
-        
-        var user = new ApplicationUser
-        {
-            UserName = registerInput.UserName,
-            Email = registerInput.Email,
-            EmailConfirmed = true
-        }; 
-        
-        var userManagerMock = new Mock<UserManager<ApplicationUser>>(
-            Mock.Of<IUserStore<ApplicationUser>>(),
-            null, null, null, null, null, null, null, null);
-        
-        userManagerMock.Setup(m => m.CreateAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>()))
-            .ReturnsAsync(IdentityResult.Failed(new IdentityError { Code = "InvalidEmailFormat", Description = "Email is in an invalid format." }));
-
-        var signInManagerMock = new Mock<SignInManager<ApplicationUser>>(
-            userManagerMock.Object, Mock.Of<IHttpContextAccessor>(), 
-            Mock.Of<IUserClaimsPrincipalFactory<ApplicationUser>>(), 
-            null, null, null, null);
-
-        var jwtOptionsMock = new Mock<IOptions<JwtOptions>>();
-        var jwtOptions = new JwtOptions
-        {
-            Issuer = "UnitTests",
-            Audience = "UnitTests",
-            SigningCredentials = null,
-            Expiration = 0
-        };
-        jwtOptionsMock.Setup(o => o.Value).Returns(jwtOptions);
-        
-        var identityService = new IdentityService(signInManagerMock.Object, userManagerMock.Object,  jwtOptionsMock.Object);
-        
-        //Act
-        var result = await identityService.Register(registerInput);
+        var utils = IdentityServiceTestsUtils.NewUtils();
+        var identityService = new IdentityService(utils.SinInManagerMock.Object, utils.UserManagerMock.Object,  utils.JwtOptionsMock.Object);
+       
+        var result = await identityService.Register(utils.RegisterInput);
 
         //Assert
         result.Errors!.Count.Should().BeGreaterThan(0);
         result.Success.Should().BeFalse();
 
-        userManagerMock.Verify(m =>
+        utils.UserManagerMock.Verify(m =>
             m.CreateAsync(It.Is<ApplicationUser>(u =>
-                    u.UserName!.Equals(user.UserName) && u.Email.Equals(user.Email) && u.UserName.Equals(user.UserName) && u.TwoFactorEnabled == user.TwoFactorEnabled),
-                It.Is<string>(p => p.Equals(registerInput.Password))), Times.Once);
+                    u.UserName!.Equals(utils.Users.UserName) && u.Email.Equals(utils.Users.Email) && u.UserName.Equals(utils.Users.UserName) && u.TwoFactorEnabled == utils.Users.TwoFactorEnabled),
+                It.Is<string>(p => p.Equals(utils.RegisterInput.Password))), Times.Once);
         
-        userManagerMock.Verify(m => 
+        utils.UserManagerMock.Verify(m => 
             m.SetLockoutEnabledAsync(It.IsAny<ApplicationUser>(), It.IsAny<bool>()), Times.Never);
     }
     
--- Coders-Back/Coders-Back.UnitTest/Infrastructure/IdentityServiceTestsUtils.cs
+++ Coders-Back/Coders-Back.UnitTest/Infrastructure/IdentityServiceTestsUtils.cs
@@ -1,6 +1,60 @@
+using Coders_Back.Domain.DTOs.Input;
+using Coders_Back.Domain.Entities;
+using Coders_Back.Infrastructure.Identity.Configurations;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Moq;
+
 namespace Coders_Back.UnitTest.Infrastructure;
 
 public class IdentityServiceTestsUtils
 {
+    public RegisterInput RegisterInput { get; set; }
+    public ApplicationUser Users { get; set; }
+    public Mock<UserManager<ApplicationUser>> UserManagerMock  { get; set; }
+    public Mock<SignInManager<ApplicationUser>> SinInManagerMock { get; set; }
+    public Mock<IOptions<JwtOptions>>  JwtOptionsMock { get; set; }
     
+    public static IdentityServiceTestsUtils NewUtils()
+    {
+        return new IdentityServiceTestsUtils();
+    }
+
+    private IdentityServiceTestsUtils()
+    {
+        RegisterInput = new RegisterInput("amazingemail.something", "An amazing person", "AInvalidPassword",
+            "AInvalidPassword");
+        
+        Users = new ApplicationUser
+        {
+            UserName = "An amazing person",
+            Email = "amazingemail.something",
+            EmailConfirmed = true
+        }; 
+        
+        UserManagerMock = new Mock<UserManager<ApplicationUser>>(
+            Mock.Of<IUserStore<ApplicationUser>>(),
+            null, null, null, null, null, null, null, null);
+        
+        UserManagerMock.Setup(m => m.CreateAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>()))
+            .ReturnsAsync(IdentityResult.Failed(new IdentityError { Code = "InvalidEmailFormat", Description = "Email is in an invalid format." }));
+        
+        SinInManagerMock = new Mock<SignInManager<ApplicationUser>>(
+            UserManagerMock.Object, Mock.Of<IHttpContextAccessor>(), 
+            Mock.Of<IUserClaimsPrincipalFactory<ApplicationUser>>(), 
+            null, null, null, null);
+        
+        JwtOptionsMock = new Mock<IOptions<JwtOptions>>();
+        
+        var jwtOptions = new JwtOptions
+        {
+            Issuer = "UnitTests",
+            Audience = "UnitTests",
+            SigningCredentials = null,
+            Expiration = 0
+        };
+        
+        JwtOptionsMock.Setup(o => o.Value).Returns(jwtOptions);
+    }
 }
