3fc2263e1e4bdcedae88f323887c0710aa47acd2
Jonathan Maia <jtauan10@gmail.com>
2023-06-14 17:52:36 -0300

    feat.39: configuring UnitTestBase for use in memory db on unit tests

---
+++ Coders-Back/Coders-Back.UnitTest/UnitTestBase.cs
@@ -0,0 +1,40 @@
+using Coders_Back.Domain.DataAbstractions;
+using Coders_Back.Infrastructure.DataAbstractions;
+using Coders_Back.Infrastructure.EntityFramework.Context;
+using Microsoft.EntityFrameworkCore;
+using Xunit.Sdk;
+
+namespace Coders_Back.UnitTest
+{
+    public class UnitTestBase
+    {
+        private AppDbContext? Context { get; }
+
+        protected UnitTestBase()
+        {
+            var builder = new DbContextOptionsBuilder<AppDbContext>();
+            builder.UseInMemoryDatabase("AppDbContextInMemory");
+            var options = builder.Options;
+            Context = new AppDbContext(options);
+            Context.Database.EnsureDeleted();
+            Context.Database.EnsureCreated();
+        }
+
+        protected IRepository<TEntity> GetInMemoryRepository<TEntity>() where TEntity : class
+        {
+            if (Context is not null) return new Repository<TEntity>(Context);
+            throw new NullException(typeof(AppDbContext));
+        }
+        
+        protected IUnitOfWork GetInMemoryUnitOfWork()
+        {
+            if (Context is not null) return new UnitOfWork(Context);
+            throw new NullException(typeof(AppDbContext));
+        }
+        
+        public void Dispose()
+        {
+            Context?.Database.EnsureDeleted();
+        }
+    }
+}
