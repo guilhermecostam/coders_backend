9c8d0afeac6dbbbcb10d5c1f0946e67cb26cbc15
Jonathan Maia <jtauan10@gmail.com>
2023-06-13 21:48:27 -0300

    feat.17: logic implementation

--- Coders-Back/Coders-Back.Domain/Services/ProjectService.cs
+++ Coders-Back/Coders-Back.Domain/Services/ProjectService.cs
@@ -3,47 +3,69 @@ using Coders_Back.Domain.DTOs.Input;
 using Coders_Back.Domain.DTOs.Output;
 using Coders_Back.Domain.Entities;
 using Coders_Back.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Coders_Back.Domain.Services;
 
 public class ProjectService : IProjectService
 {
+    private readonly IGithubApi _githubApi;
     private readonly IRepository<Project> _projects;
     private readonly IRepository<ProjectJoinRequest> _requests;
     private readonly IRepository<ApplicationUser> _users;
     private readonly IRepository<Collaborator> _collaborators;
     private readonly IUnitOfWork _unitOfWork;
 
-    public ProjectService(IRepository<Project> projects, IRepository<ProjectJoinRequest> requests, IRepository<ApplicationUser> users, IUnitOfWork unitOfWork, IRepository<Collaborator> collaborators)
+    public ProjectService(IRepository<Project> projects, IRepository<ProjectJoinRequest> requests, IRepository<ApplicationUser> users, IUnitOfWork unitOfWork, IRepository<Collaborator> collaborators, IGithubApi githubApi)
     {
         _projects = projects;
         _requests = requests;
         _users = users;
         _unitOfWork = unitOfWork;
         _collaborators = collaborators;
+        _githubApi = githubApi;
     }
 
     public async Task<List<ProjectOutput>> GetAll()
     {
-        var projects = await _projects.GetAll();
-        return projects.Select(p => new ProjectOutput(p)).ToList();
+        var usersDbSet = _users.GetDbSet();
+        var projectsDbSet = _projects.GetDbSet();
+        var query = from project in projectsDbSet
+            join user in usersDbSet on project.OwnerId equals user.Id
+            select new
+            {
+                project,
+                userGhName = user.GithubProfile
+            };
+        var projects = await query.ToListAsync();
+        return projects.Select( async projectAndName =>
+        {
+            projectAndName.project.Technologies = await _githubApi.GetTechnologiesByProject(projectAndName.userGhName, GetGhRepositoryNameByUrl(projectAndName.project.GithubUrl));
+            return new ProjectOutput(projectAndName.project);
+        }).Select(task => task.Result).ToList();
     }
 
     public async Task<ProjectOutput?> GetById(Guid projectId)
     {
         var project = await _projects.GetById(projectId);
-        return project is null ? null : new ProjectOutput(project);
+        if (project is null) return null;
+        var user = await _users.GetById(project.OwnerId);
+        project.Technologies = await _githubApi.GetTechnologiesByProject(user!.GithubProfile, GetGhRepositoryNameByUrl(project.GithubUrl));
+        var projectOutput = new ProjectOutput(project);
+        return projectOutput;
     }
 
     public async Task<ProjectOutput> CreateProject(ProjectInput projectInput, Guid ownerId)
     {
-        var project = new Project{
+        var project = new Project
+        {
             Name = projectInput.Name,
             Description = projectInput.Description,
             GithubUrl = projectInput.GithubUrl,
             DiscordUrl = projectInput.DiscordUrl,
             OwnerId = ownerId
         };
+        //Fetch Technologies from Github and save them in cache
 
         await _projects.Insert(project);
         await _collaborators.Insert(new Collaborator
@@ -120,5 +142,12 @@ public class ProjectService : IProjectService
         var project = await _projects.GetById(projectId);
         if (project is null) return false;
         return project.OwnerId == userId;
+    }
+
+    private static string? GetGhRepositoryNameByUrl(string? repoUrl)
+    {
+        if (string.IsNullOrEmpty(repoUrl)) return null;
+        var url = repoUrl.Split('/');
+        return url[^1];
     }
 }
