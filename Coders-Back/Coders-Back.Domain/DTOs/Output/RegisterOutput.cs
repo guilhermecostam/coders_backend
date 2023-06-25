9364784c8a2aff39c4b6f37263c434567dd82be5
Rusdrael Antony <rusdrael1@hotmail.com>
2023-06-25 01:40:14 -0300

    feat.44: Added in RegisterOutput.cs "ConfirmationEmailToken" and "UserId"

--- Coders-Back/Coders-Back.Domain/DTOs/Output/RegisterOutput.cs
+++ Coders-Back/Coders-Back.Domain/DTOs/Output/RegisterOutput.cs
@@ -8,4 +8,10 @@ public class RegisterOutput
 
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public List<string>? Errors { get; set; }
+    
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? ConfirmationEmailToken { get; set; } // TODO: Remover essa propriedade 
+    
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public Guid? UserId { get; set; }
 }
