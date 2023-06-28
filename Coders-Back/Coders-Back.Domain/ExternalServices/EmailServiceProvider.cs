cf31b502a25b9536cb32e9133d632edb94d9980a
Jonathan Maia <jtauan10@gmail.com>
2023-06-28 18:41:48 -0300

    fix

--- Coders-Back/Coders-Back.Domain/ExternalServices/EmailServiceProvider.cs
+++ Coders-Back/Coders-Back.Domain/ExternalServices/EmailServiceProvider.cs
@@ -3,23 +3,30 @@ using System.Net.Mail;
 using Coders_Back.Domain.DTOs.Input;
 using Coders_Back.Domain.Interfaces;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace Coders_Back.Domain.ExternalServices;
 
 public class EmailServiceProvider : IEmailServiceProvider
 {
-    public EmailServiceProvider(IConfiguration configuration)
+    private readonly ILogger<EmailServiceProvider> _logger;
+
+    public EmailServiceProvider(IConfiguration configuration, ILogger<EmailServiceProvider> logger)
     {
-        _pass = Environment.GetEnvironmentVariable("SMTP_PASSWORD") ?? configuration["EmailProvider:Password"];
+        _logger = logger;
+        _password = Environment.GetEnvironmentVariable("SMTP_PASSWORD") ?? configuration["EmailProvider:Password"];
     }
     
     private const string Email = "codersproject@outlook.com";
     private const string Host = "smtp.office365.com";
     private const int Port = 587;
-    private static string? _pass;
+    private static string? _password;
     
     public void SendEmail(SendEmailInput input)
     {
+        if (_password is null)
+            _logger.LogError("SMTP password not found. If running for development," +
+                             " check appsettings.json or docker-compose.yml and correct");
         
         var message = new MailMessage
         {
@@ -38,7 +45,7 @@ public class EmailServiceProvider : IEmailServiceProvider
         
         var smtpClient = new SmtpClient
         {
-            Credentials = new NetworkCredential(Email, _pass),
+            Credentials = new NetworkCredential(Email, _password),
             EnableSsl = input.EnableSsl,
             Host = Host,
             Port = Port,
